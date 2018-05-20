68d4d32a67105c6dbf95227525e8d208d786b947
BravooZ <39419222+BravooZ@users.noreply.github.com>
2018-05-20 14:56:12 +0100

    Create setautorole.js

---
+++ commands/setautorole.js
@@ -0,0 +1,14 @@
+const db = require('quick.db')
+
+exports.run = (bot, message, args, func) => {
+
+    if (!message.member.hasPermission('👑Fundador👑')) return message.channel.send('Isso requer que você tenha a role com `👑Fundador👑`') // Tell them that they dont have the proper perms
+    if (!args.join(" ")) return message.channel.send('Por favor insira argumentos. `setAutoRole <roleName>`') // Tell them if they didn't supply arguments
+
+    db.updateText(`autoRole_${message.guild.id}`, args.join(" ").trim()).then(i => { // .trim() removes the whitespaces on both ends of the string.
+
+        message.channel.send('Função automática alterada com sucesso para: `' + i.text + '`'); // This tells them what they just set the autorole to.
+
+    })
+
+}
